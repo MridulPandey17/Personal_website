832481b3d81cddd9b3f1ad61211f6d5849aa1ebf
MridulPandey17 <104188420+MridulPandey17@users.noreply.github.com>
2025-01-01 05:18:00 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from '@astro/adapters/netlify';
-
-export default {
-  site: 'https://your-site-name.netlify.app',  // Replace with your Netlify URL
-  base: '/',  // Set to '/' if deploying at the root
-  adapter: netlify(), // Ensure you're using the Netlify adapter
-  output: 'static',  // Specify the output type
-};
+export default defineConfig({
+  site: 'https://astrofy-template.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
