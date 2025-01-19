ca6363dd4785b77be0b9ef29ee6561dfdfcbd3aa
Mridul_Pandey <test@mridulpandey417@gmail.com>
2025-01-19 19:00:59 +0530

    final_01

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()],
+});
 
 // https://astro.build/config
 export default defineConfig({
